68fc3cc0061a5166e52549eaa8f3bb51b7fe8044
Alexander Higgins <65186976+alhigginsucf@users.noreply.github.com>
2021-01-07 17:29:08 -0500

    Created basic navbar with Main Page and Create Post. Set up Router in React App. Set up PORT: 3001 on server side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect} from "react";
-import './App.css';
-import Axios from "axios";
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "./App.css";
+import MainPage from "./pages/MainPage";
+import CreatePost from "./pages/CreatePost";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="navbar">
+        <div className="links">
+          <a href="/">Main Page</a>
+          <a href="/createpost">Create Post</a>
+        </div>
+      </div>
+
+      <Router>
+        <Route path="/" exact render={(props) => <MainPage />}/>
+        <Route path="/createpost" render={(props) => <CreatePost />} />
+      </Router>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -3,9 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql');
 
+const PORT = 3001
+
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'cruddatabase',
-})
+});
+
+app.get('/', (req, res)=> {
+    res.send('hello world');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+});
